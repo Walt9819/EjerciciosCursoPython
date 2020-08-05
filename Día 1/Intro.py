28ee8b1cb68c9248d7dfc021702e048a03a3d4c8
walt22r <walter22r@icloud.com>
2020-08-05 14:19:05 -0500

    Modificaciones a los parámetros de diagnóstico

--- Día 1/Intro.py
+++ Día 1/Intro.py
@@ -1,6 +1,7 @@
 from Ayudas import ObtenerNumeroPacientes, PacienteTiene
 
-def Diagnostico(sintomas):
+def Diagnostico(tos, fiebre, dolor_cabeza, dolor_garganta,
+                ojos_rojos, malestar_general, dificultad_respirar):
     # TODO
 
 no_pacientes = ObtenerNumeroPacientes()
